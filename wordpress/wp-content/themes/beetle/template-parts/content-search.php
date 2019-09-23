180519238962b88f9fa3739dc6078998a799be50
yuanyi.lp <yuanyi.lp@alibaba-inc.com>
2019-09-23 15:28:04 +0800

    feature(wordpress): add beetle theme

---
+++ wordpress/wp-content/themes/beetle/template-parts/content-search.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying articles in the search loop
+ *
+ * @package Beetle
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<header class="entry-header">
+
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+	</header><!-- .entry-header -->
+
+	<div class="entry-content clearfix">
+		<?php the_excerpt(); ?>
+		<?php beetle_more_link(); ?>
+	</div><!-- .entry-content -->
+
+</article>
